6f4b890df981f0252642d9cd2353b2fc2951793a
Alex Dobrynin <shuron1988@gmail.com>
2025-01-24 20:28:45 +0200

    fixed build script

--- NukeProxy/build.sh
+++ NukeProxy/build.sh
@@ -5,7 +5,7 @@ xcodebuild archive -destination "generic/platform=iOS" -project NukeProxy.xcodep
 
 xcodebuild archive -destination "generic/platform=iOS Simulator" -project NukeProxy.xcodeproj -scheme NukeProxy -configuration Release -archivePath Output/Output-iphonesimulator SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-xcodebuild archive -destination "generic/platform=macOS,vairant=Mac Catalyst" -project NukeProxy.xcodeproj -scheme NukeProxy -configuration Release -archivePath Output/Output-maccatalyst SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+xcodebuild archive -destination "generic/platform=macOS,variant=Mac Catalyst" -project NukeProxy.xcodeproj -scheme NukeProxy -configuration Release -archivePath Output/Output-maccatalyst SKIP_INSTALL=NO ENABLE_BITCODE=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 echo "create xcframework"
 xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framework NukeProxy.framework -archive Output/Output-iphonesimulator.xcarchive -framework NukeProxy.framework -output Output/NukeProxy-ios.xcframework
@@ -13,4 +13,4 @@ xcodebuild -create-xcframework -archive Output/Output-iphoneos.xcarchive -framew
 xcodebuild -create-xcframework -archive Output/Output-maccatalyst.xcarchive -framework NukeProxy.framework -output Output/NukeProxy-maccatalyst.xcframework
 
 echo "sharpie bind"
-sharpie bind --sdk=iphoneos17.5 --output="Output" --namespace="MPowerKit.NukeProxy" --scope="Output/NukeProxy.xcframework/ios-arm64/NukeProxy-ios.framework/Headers" "Output/NukeProxy-ios.xcframework/ios-arm64/NukeProxy.framework/Headers/NukeProxy-Swift.h"
+sharpie bind --sdk=iphoneos18.2 --output="Output" --namespace="MPowerKit.NukeProxy" --scope="Output/NukeProxy.xcframework/ios-arm64/NukeProxy-ios.framework/Headers" "Output/NukeProxy-ios.xcframework/ios-arm64/NukeProxy.framework/Headers/NukeProxy-Swift.h"
